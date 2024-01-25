9d3251f52f2362e2730c1e8f51d832bf3a76033b
Jordan <jgcomputers132@gmail.com>
2024-01-25 09:01:48 -0500

    Strings

---
+++ Strings/first.py
@@ -0,0 +1,13 @@
+#!/bin/python3
+
+#Programmer: Jordan Gibbs
+#Program: Strings
+
+print("Hello World\n")
+
+print("7 + 8 =",7+8)
+print("")
+print("9 + 3 = "+ str(9+3),"\n")
+
+print("""Happy New Year!
+everyone, Yeah!!!""") #"""will print on muiltiple lines"""
