ed23cb3d9a8b5c7e9b890c1b1d391a9546cb6894
Jordan <jgcomputers132@gmail.com>
2024-01-25 09:05:13 -0500

    Math

---
+++ Math/math.py
@@ -0,0 +1,12 @@
+#Programmer: Jordan Gibbs
+#Program: Math
+#Date: 1.10.2024
+
+print(9 + 9) #Addition
+print(9 * 9) #Muiltiplication
+print(9 - 9) #Subtraction
+print(9 / 9) #Divide
+print(9 ** 3) #Exponent - the ** makes an exponent 9^3
+print(50 % 6) #Modulas - Takes the remainder number. The number 6 can go into 50, 8 times which = 48 leaving the remainder 2
+print(50 / 6) #Division with remainder - Float
+print(50 // 6) #The // will give the whole number without remainder (a decimal)
